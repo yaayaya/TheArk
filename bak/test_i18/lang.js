247b2e9dd22b36b51576470c9201d0215a283e5d
wade.chen <wade.chen@ecpay.com.tw>
2021-08-19 13:24:42 +0800

    更換語言套件  模型更新

--- bak/test_i18/lang.js
+++ bak/test_i18/lang.js
@@ -1,13 +1,25 @@
 let zhtw = {
-    title: {
-      hello: 'testw、世界'
+  translation: {
+    input: {
+      placeholder: "a placeholder"
+    },
+    nav: {
+      home: 'Home',
+      page1: 'Page One',
+      page2: 'Page Two'
     }
-  
+  }
 }
 
 let en = {
-  title: {
-      hello: 'ttttttttttttttttttttt'
+  translation: {
+    input: {
+      placeholder: "a placasdfsavcsvseholder"
+    },
+    nav: {
+      home: 'Hocccsvcdsvme',
+      page1: 'Pageasdvsadv One',
+      page2: 'Pageasdvsadc Two'
     }
-  
+  }
 }
